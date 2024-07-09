a248152e193331ae0bda886d770a1b2af15c84f1
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-07-09 12:19:48 +0530

    Time: 77 ms (49.55%), Space: 18 MB (38.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,11 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        n=len(nums)
-        dup = 0
+        curr = 1
         for i in range(1,len(nums)):
-            if nums[i-1]==nums[i]:
-                nums[i-1]=-101
-                dup+=1
-        for i in range(dup):
-            nums.remove(-101)
-        return len(nums)
+            if nums[i-1]!=nums[i]:
+                nums[curr]=nums[i]
+                curr+=1
+        return curr
