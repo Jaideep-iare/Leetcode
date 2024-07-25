3caa48c07bce4aee2f518fcdf812dfd9f995bf8b
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-07-25 20:16:08 +0530

    Time: 106 ms (80.71%), Space: 17.8 MB (46.06%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,3 +4,12 @@ class Solution:
         actualsum = (n*(n+1))//2
         butsum = sum(nums)
         return actualsum-butsum
+        # n=len(nums)
+        # xor1 = 0
+        # xor2 = 0
+        # for i in range(n):
+        #     xor1 = xor1^i
+        #     xor2 = xor2^nums[i]
+        # xor1 = xor1^n
+        # return xor1^xor2
+        
