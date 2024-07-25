fd17ab022c43ba1bb7174a1121fe56c894fef1c0
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-07-25 20:15:39 +0530

    Time: 106 ms (80.71%), Space: 17.7 MB (87.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # n=len(nums)
-        # xor1 = 0
-        # xor2 = 0
-        # for i in range(n):
-        #     xor1 = xor1^i
-        #     xor2 = xor2^nums[i]
-        # xor1 = xor1^n
-        # return xor1^xor2
         n = len(nums)
         actualsum = (n*(n+1))//2
         butsum = sum(nums)
