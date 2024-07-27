c996dfb29249ee8b1471cfbc1206d2b8b22f4cb1
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-07-27 21:38:26 +0530

    Time: 969 ms (96.88%), Space: 46.9 MB (77.16%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,13 +1,13 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        l=[]
+        l=[0]*len(nums)
         evi=0
         odi=1
         for i in nums:
             if i>0:
-                l.insert(evi,i)
+                l[evi]=i
                 evi+=2
             else:
-                l.insert(odi,i)
+                l[odi]=i
                 odi+=2
         return l
