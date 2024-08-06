4596165e30d5ed5e1047e9a535b517c16a2487dd
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-08-06 21:53:39 +0530

    Time: 101 ms (67.03%), Space: 17.4 MB (84.40%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,14 +5,36 @@ class Solution:
         """
         r=len(matrix)
         c=len(matrix[0])
-        row=[0]*r
-        col=[0]*c
+        col0=1
         for i in range(r):
             for j in range(c):
                 if matrix[i][j]==0:
-                    row[i]=1
-                    col[j]=1
-        for i in range(r):
+                    matrix[i][0]=0
+                    if j!=0:
+                        matrix[0][j]=0
+                    else:
+                        col0=0
+        for i in range(1,r):
+            for j in range(1,c):
+                if matrix[i][j]!=0:
+                    if matrix[0][j]==0 or matrix[i][0]==0:
+                        matrix[i][j]=0
+        if matrix[0][0]==0:
             for j in range(c):
-                if row[i]==1 or col[j]==1:
-                    matrix[i][j]=0
+                matrix[0][j]=0
+        if col0==0:
+            for i in range(r):
+                matrix[i][0]=0
+        # r=len(matrix)
+        # c=len(matrix[0])
+        # row=[0]*r
+        # col=[0]*c
+        # for i in range(r):
+        #     for j in range(c):
+        #         if matrix[i][j]==0:
+        #             row[i]=1
+        #             col[j]=1
+        # for i in range(r):
+        #     for j in range(c):
+        #         if row[i]==1 or col[j]==1:
+        #             matrix[i][j]=0
