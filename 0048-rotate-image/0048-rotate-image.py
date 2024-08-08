b995e73d75f23013831287013fd39c4db45544b3
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-08-08 18:53:45 +0530

    Time: 37 ms (68.02%), Space: 16.6 MB (40.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,14 +3,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m=[]
-        #bruteforce 
-        for j in range(len(matrix[0])):
         
-            row=[]
-            for i in range(len(matrix)-1,-1,-1):
-                row.append(matrix[i][j])
-            m.append(row)
+        #optimal
         for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                matrix[i][j]=m[i][j]
+            for j in range(i+1,len(matrix[0])):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        for i in range(len(matrix)):
+            matrix[i]=matrix[i][::-1]
+        
+        #bruteforce 
+#         m=[]
+#         for j in range(len(matrix[0])):
+        
+#             row=[]
+#             for i in range(len(matrix)-1,-1,-1):
+#                 row.append(matrix[i][j])
+#             m.append(row)
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 matrix[i][j]=m[i][j]
