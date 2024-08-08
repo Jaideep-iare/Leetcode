e778f86dd71b1442e4b91c4957f84b0108b05c2d
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-08-08 19:23:03 +0530

    Time: 44 ms (20.11%), Space: 16.5 MB (40.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -10,7 +10,7 @@ class Solution:
         for i in range(len(matrix)):
             matrix[i].reverse()
             
-#         #best
+#         #optimal
 #         for i in range(len(matrix)):
 #             for j in range(i+1,len(matrix[0])):
 #                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
