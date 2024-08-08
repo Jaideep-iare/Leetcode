67fae4220c1d5c56adf9eb877aa5a40f89a57aa9
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-08-08 22:37:23 +0530

    Time: 229 ms (54.78%), Space: 19.4 MB (53.35%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,16 @@
-from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        h=defaultdict(int)
-        h[0]=1
+        h={0:1}
         i=0
         preSum=0
         ans=0
         while i<len(nums):
             preSum+=nums[i]
-            comp = preSum-k
-            ans+=h[comp]
-            h[preSum]+=1
+            ans+=h.get(preSum-k,0)
+            if preSum in h:
+                h[preSum]+=1
+            else:
+                h[preSum]=1
             i+=1
 
         return ans
