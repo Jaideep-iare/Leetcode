e757686c72fdb8f5fe536c45ae71432b95da836a
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-08-08 19:16:43 +0530

    Time: 42 ms (31.78%), Space: 16.5 MB (40.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -9,7 +9,7 @@ class Solution:
             for j in range(i+1,len(matrix[0])):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
         for i in range(len(matrix)):
-            matrix[i]=matrix[i][::-1]
+            matrix[i].reverse()
         
         #bruteforce 
 #         m=[]
