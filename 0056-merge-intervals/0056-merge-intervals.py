2958080c3849ad9867052545ce7def06751ba68d
Jaideep-iare <125242278+Jaideep-iare@users.noreply.github.com>
2024-08-10 22:21:45 +0530

    Time: 121 ms (69.04%), Space: 21.1 MB (30.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,13 +3,11 @@ class Solution:
         ans=[]
         intervals.sort()
         for i in range(len(intervals)):
-            x1=intervals[i][0]
-            y1=intervals[i][1]
-            if ans and ans[-1][1]>y1:
+            if ans and ans[-1][1]>intervals[i][1]:
                 continue
-            if ans and ans[-1][1]>=x1:
-                ans[-1][1]=max(ans[-1][1], y1)
+            if ans and ans[-1][1]>=intervals[i][0]:
+                ans[-1][1]=max(ans[-1][1], intervals[i][1])
             
             else:
-                ans.append([x1,y1])
+                ans.append([intervals[i][0],intervals[i][1]])
         return ans
